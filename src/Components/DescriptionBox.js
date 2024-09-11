5e864d8433c9ccf2b4fcf41089eef21dd4887c52
mUZImil <jblhbk629@gmail.com>
2024-09-11 15:18:11 +0500

    upate

--- src/Components/DescriptionBox.js
+++ src/Components/DescriptionBox.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-export default function DescriptionBox() {
+export default function DescriptionBox({label,description}) {
   return (
-    <div className='descriptionBox p-2 border-b border-[#c9c9c9]'>
+    <div className='descriptionBox p-4 border-b border-[#c9c9c9]'>
         <div className='flex justify-between pb-4'>
-            <div className='px-3 py-1 rounded-full bg-[#0d67a5] text-white'><h4>USES</h4></div>
+            <div className='px-3 py-1 rounded-full bg-[#0d67a5] text-white'><h4>{label}</h4></div>
         </div>
         <div>
             <h4>
-            Jhang is most famous for the legendary love story of Heer and Ranjha. The tomb of Heer and Ranjha, located in the heart of the city, draws visitors from all over who come to pay homage to the couple whose love defied social norms and has been immortalized in Punjabi folklore."
+            {description}
             </h4>
         </div>
     </div>
